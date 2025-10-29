2a646f36f81afd77398644e079f9dce951db3a6c
cag-uconn <khan@uconn.edu>
2025-10-29 11:42:09 -0400

    .

---
+++ pa4/src/sim_stages.c
@@ -0,0 +1,96 @@
+/**
+ * University of Connecticut
+ * CSE 4302: Computer Architecture
+ * Fall 2025
+ * 
+ * riscy-uconn: sim_stages.c
+ * 
+ */
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "instruction_map.h"
+#include "sim_core.h"
+#include "sim_stages.h"
+#include "decode_fields.h"
+
+/**
+ * Debug flags
+ */
+int debug = 0;           /* Set to 1 for additional debugging information. */
+int pipe_trace = 1;      /* Set to 1 for pipe trace. */
+int pipe_trace_mode = 3; /* See PA1 handout, section 5 for usage */
+
+
+
+/**
+ * Fetch stage implementation.
+ */
+void fetch() {
+
+    /*Insert your code here*/
+   
+}
+
+/**
+ * Decode stage implementation
+ */
+void decode() {
+    
+    struct State *older;
+    struct State *younger;
+
+    /*Insert your code here*/
+   
+
+}
+
+/**
+ * Execute stage implementation
+ */
+struct State execute() {
+
+     /*Insert your code here*/
+    
+}
+
+
+
+/**
+ * Execute stage implementation for Load 
+ */
+struct State load_stage() {
+    
+    /*Insert your code here*/
+
+}
+
+
+/**
+ * Execute stage implementation for Stores
+ */
+struct State store_stage() {
+    
+    /*Insert your code here*/
+    
+}
+
+/**
+ * Writeback stage implementation
+ */
+void writeback() {
+
+     /*Insert your code here*/
+ 
+}
+
+/**
+ * Advance PC.
+ * DO NOT MODIFY.
+ */
+
+void advance_pc(int step) {
+    pc_n = step;
+}
